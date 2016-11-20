7fbf30d55e71c9a70b2710a4a67d4e5c0a790c79
Haydz <haydnit@gmail.com>
2016-11-20 13:08:18 -0500

    Fixed adding unique hosts and writing to file. Pretty much finished.

--- Arp_ip_finder/arp_scanner.py
+++ Arp_ip_finder/arp_scanner.py
@@ -13,8 +13,8 @@ def scan(command):
 def BaseLineTest():
 	print "++++++++++++++++++++++++++++++++"
 	print "+++RUNNING BASELINE CHECKS +++++"
-	print "+++ ROOT | BaseDir etc     +++++"
-	print "++++++++++++++++++++++++++++++++"
+	print "+++ ROOT | Base Directory etc+++++"
+	print "++++++++++++++++++++++++++++++++\n "
 	# Checking Running as root, for write perms
 	try:
 		checkPermissions = 'whoami'
@@ -32,23 +32,25 @@ def BaseLineTest():
 	#finding full full path of script
 	checkPath = 'pwd'
 	FullPath = scan(checkPath).strip() + '/'
-	print "The Full path to be used for script is ", FullPath
+	#print "The Full path to be used for the script is ", FullPath
 
 
-	baseDir = "base"
+	baseDir = "ArpHosts"
 	# create base sub dir to place all files
 	try:
 		mkBaseDir = "mkdir %s" % baseDir
 		scan(mkBaseDir)
 	except Exception as e:
-		print e
+		print e, "\n"
 
 	# check dir was created
-	checkDir = "ls | grep base"
-	checkDirDirResults = scan(checkDir)
-	if 'base' in checkDirDirResults:
-		print "BASE directory found.. continuing"
-
+	try:
+		checkDir = "ls | grep %s" % baseDir
+		checkDirDirResults = scan(checkDir)
+		if baseDir in checkDirDirResults:
+			print "BASE directory found.. continuing"
+	except Exception as e:
+		print e
 	FullPath = ''.join((FullPath, baseDir)) + '/'
 	print "Full path and baseDir for script will be ", FullPath
 	return FullPath
@@ -73,25 +75,33 @@ def arpPing(IPRange):
 	ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=IPRange), timeout=5)
 	ans.summary(lambda (s,r):  r.sprintf("%Ether.src% %ARP.psrc%"))
 	for snd, rcv in ans:
-		print rcv.sprintf(r"%Ether.src% & %ARP.psrc%\\")
+		print "Ethernet Address and IP Addresses of hosts: "
+		print rcv.sprintf(r"%Ether.src% & %ARP.psrc%")
 		IP = rcv.sprintf("%ARP.psrc%")
-		arpPingIPList.append(IP)
-		print IP
+		if IP not in arpPingIPList:
+			arpPingIPList.append(IP)
+		#print IP
 
 	return arpPingIPList
-"""====START OF MAIN ====="""
+
+
 
 if __name__ == '__main__':
+	"""====START OF MAIN ====="""
+
 
 	print "++++ This Script will identify IP addresses from ARP PACKETS ++++"
 	print "++++ and it will run an ArpPing Scan to be more acive        ++++"
 	print "Press Enter to continue, or press CTRL C to quit\n"
 	raw_input("> ")
 
+	seconds = int(raw_input("Please enter the amount of seconds to Sniff ARP packerts for"
+				  "> "))
 
-	seconds = 2
+
+	#seconds = 2
 	bpf = 'arp'
-	outPutFile = 'testip.txt'
+	outPutFile = 'ArpHosts.txt'
 	IPlist = []
 	FullPath = BaseLineTest()
 	time.sleep(2)
@@ -103,51 +113,35 @@ if __name__ == '__main__':
 
 
 	#print "Running Arp Ping"
-	arpPingResults = arpPing("10.0.9.*")
+	arpPingResults = arpPing("10.255.252.*")
 	print "ARP PING RESULTS "
 	print arpPingResults
-	#URL TO HELP http://www.secdev.org/projects/scapy/build_your_own_tools.html
+	print IPlist
 
-	#arpPingResults.summary(lambda (s,r):  r.sprintf("%ARP.psrc%"))
-	#print "RESULTS", results
-	#x = arping("10.0.9.*")
-	#print "x" , x
-	print "Sniffer finished"
-	#print "IP LIST:"
-	#print IPlist
+	for arpPingIP in arpPingResults:
+		if arpPingIP not in IPlist:
+			print "Adding IP from Arp Ping scan to list: ", arpPingIP
+			IPlist.append(arpPingIP)
+	print "IPLIST: ", IPlist
+
+	print "====Sniffer finished====\n"
 
 	FileFullPath = FullPath + outPutFile
 
 	#Creating blank file
 	ipFile = open(FileFullPath, 'w').close()
 
-	print "[!] Writing contents to %s" % outPutFile
+
 	if IPlist:
+		print "====Writing IPs found to file"
+		print "[!] Writing contents to %s" % outPutFile
 		ipFile = open(FileFullPath, 'w')
 		ipFile.write("===Hosts identified via ARP Sniffing==" '\n')
 		for IP in IPlist:
-			print "Found Host: %s, writing to file" % IP
+			print "Found Host: %s writing to file" % IP
 			ipFile.write(IP + '\n')
 		ipFile.close()
+	else:
+		print "NO IP ADDRESSES FOUND"
 
 
-"""
-
-
-ARP Ping
-
-The fastest way to discover hosts on a local ethernet network is to use the ARP Ping method:
-
->>> ans,unans=srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="192.168.0.0/24"),timeout=2)
-
-Answers can be reviewed with the following command:
-
->>> ans.summary(lambda (s,r): r.sprintf("%Ether.src% %ARP.psrc%") )
-
-Scapy also includes a built-in arping() function which performs similar to the above two commands:
-
->>> arping("192.168.1.*")
-
-
-
-"""
